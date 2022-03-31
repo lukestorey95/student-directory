0b49f3197057787a47f2cd0499cb1e034c7ba2b4
Luke Storey <lukemstorey@gmail.com>
2022-03-31 15:06:13 +0100

    Revert "Revert "Add ability to get student directory from user""
    This reverts commit 23808628fa4c977aa68301e8be356e526d2035b7.

--- directory.rb
+++ directory.rb
@@ -1,19 +1,22 @@
-# array of students
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+# Method to get students from user
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit enter twice"
 
-# define methods
+  students = []
+
+  name = gets.chomp
+
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+
+  students
+end
+
+# define methods to print student information
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -30,6 +33,7 @@ def print_footer(students)
 end
 
 # call methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
