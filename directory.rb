d5464436449b61063efca00a54991d19854c56cc
Luke Storey <lukemstorey@gmail.com>
2022-04-01 09:29:01 +0100

    Refactor code

--- directory.rb
+++ directory.rb
@@ -1,39 +1,64 @@
-# Method to get students from user
+@students = []
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit enter twice"
 
-  students = []
-
   name = gets.chomp
 
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-
-  students
 end
 
-# define methods to print student information
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_students_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
-# call methods
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+interactive_menu
+
